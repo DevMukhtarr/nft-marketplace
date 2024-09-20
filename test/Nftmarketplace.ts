e3ecf950d6a78edbceaf1eb55fe555524100fe9c
Mukhtar Abdulfatai <mukhtarabdulfatai@gmail.com>
2024-09-20 19:49:10 +0100

    adding test

--- test/Nftmarketplace.ts
+++ test/Nftmarketplace.ts
@@ -6,14 +6,53 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre from "hardhat";
 
-describe("Nftmarketplace", function () {
+describe("EventNft", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployNFT() {
+  async function deployNftAndEventContract() {
 
-    const [owner, addr1, addr2] = await hre.ethers.getSigners();
+    // Contracts are deployed using the first signer/account by default
+    const [owner, account2, account3] = await hre.ethers.getSigners();
 
-    return {  };
+    const NFT = await hre.ethers.getContractFactory("MarketplaceNFT ");
+    const nft = await NFT.deploy();
+
+    const MARKETPLACE = await hre.ethers.getContractFactory("NftMarketplace")
+    const marketplace = await MARKETPLACE.deploy(await nft.getAddress());
+
+    return { nft, owner, marketplace,account2, account3 };
   }
+
+  describe("Deployment", function () {
+    it("Should check if owner is set properly", async function () {
+      const { nft, owner } = await loadFixture(deployNftAndEventContract);
+      
+      // check is address is correct
+      expect(await nft.getAddress()).to.equal(nft.target)
+
+      // check if owner is set properly
+      expect(await nft.owner()).to.equal(owner)
+    });
+
+    it("Should check if NFT address is set properly in marketplace", async function () {
+      const { nft,marketplace } = await loadFixture(deployNftAndEventContract);
+
+      expect(await marketplace.nftAddress()).is.equal(await nft.getAddress())
+    })
+  });
+
+  it("Should check if mint is working properly", async function () {
+    const { nft, owner, account2 } = await loadFixture(deployNftAndEventContract);
+
+    await nft.mint(owner)
+    await nft.mint(account2)
+
+    expect(await nft.balanceOf(account2) > 0)
+
+    await expect(nft.mint(account2))  
+    .to
+    .emit(nft, "NftMinted")
+    .withArgs(account2, await nft.tokenId())
+  });
 });
